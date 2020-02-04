f671758c1e615e2a586656cbf3ba6e93bc102369
Guilherme Mertens <gvmertens@gmail.com>
2020-02-04 10:15:50 -0300

    Sequelize

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Guilherme' });
-});
+routes.get('/', (req, res) => res.json({ message: 'Guilherme' }));
 
 export default routes;
