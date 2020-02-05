62c3b255a798620c35b2ba2a345f7a6c847bbaaf
gvmertens <gvmertens@gmail.com>
2020-02-04 22:09:24 -0300

    Model User pronto

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Guilherme' }));
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Guilherme Mertens',
+        email: 'gvmertens@gmail.com',
+        password_hash: '123456789',
+        provider: false
+    });
+    res.json(user);
+});
 
 export default routes;
