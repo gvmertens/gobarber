87ca8b9d73fdef3cca8029ad88f0effc30f119c4
Guilherme Mertens <gvmertens@gmail.com>
2020-02-07 10:44:06 -0300

    final do cad

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,17 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// Interceptador de autenticacao
+routes.use(authMiddleware);
+
+// Rotas que serão usadas somente por usuários autenticados
+routes.put('/users', UserController.update);
 
 export default routes;
