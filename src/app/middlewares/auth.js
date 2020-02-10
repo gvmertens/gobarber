cd2568c4fd259fa7c67b079aaa54402076476b16
Guilherme Mertens <gvmertens@gmail.com>
2020-02-10 12:36:36 -0300

    ajustes valida2

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -4,16 +4,22 @@ import { promisify } from 'util';
 import authConfig from '../../config/auth';
 
 export default async (req, res, next) => {
+    console.log('Dentro do 1');
+
     const authHeader = req.headers.authorization;
     if (!authHeader) {
         return res.status(401).json({ error: 'Usuário não autenticado.' });
     }
 
     const [, token] = authHeader.split(' ');
+    console.log('Dentro do auth');
 
     try {
         const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-        req.userId = decoded.userId;
+        console.log(decoded);
+        req.userId = decoded.id;
+        // eslint-disable-next-line no-console
+        console.log(decoded);
         return next();
     } catch (err) {
         return res.status(401).json({ error: 'Usuário não autenticado.' });
